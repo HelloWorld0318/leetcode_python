ea6e89dc3fa2d7946512f9eb05ed39af2a4f7aab
houjinxiang <houjinxiang@fangdd.com>
2020-03-05 12:07:37 +0800

    https://leetcode.com/problems/reverse-linked-list/

--- problem0206/Solution.py
+++ problem0206/Solution.py
@@ -1,18 +1,35 @@
-from problem0206 import ListNode
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         if head is None or head.next is None:
             return head
-        dummy = ListNode(-1)
-        pre = head
-        cur = head.next
-        dummy.next = pre
-        while cur is not None:
-            pre.next = cur.next
-            cur.next = dummy.next
-            dummy.next = cur
+        newHead = None
+        while head is not None:
+            nextNode = head.next
+            head.next = newHead
+            newHead = head
 
-            cur = pre.next
-        return dummy.next
+            head = nextNode
+        return newHead
+
+
+if __name__ == '__main__':
+    node1 = ListNode(1)
+    node2 = ListNode(2)
+    node3 = ListNode(3)
+    node4 = ListNode(4)
+    node5 = ListNode(5)
+
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
+
+    solution = Solution()
+    node1 = solution.reverseList(node1)
+    print(node1)
